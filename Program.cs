a7f0fea4fb258a2ab9f4247b1542221990740093
Gökhan Çam <76878009+gokcam@users.noreply.github.com>
2022-02-14 20:20:21 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir cümle giriniz: ");
+            var text = Console.ReadLine();
+            char[] sesliHarfler = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+            List<char> sesliList = new List<char>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                foreach (var item in sesliHarfler)
+                {
+                    if(text[i] == item)
+                    {
+                        sesliList.Add(text[i]);
+                    }
+                }
+            }
+
+            sesliList.Sort();
+            sesliList.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
